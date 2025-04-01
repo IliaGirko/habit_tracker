9692f172e6f5e1e696acc9ed08b7e1edff3f63e4
Илья Гирько <Gggggirr@gmail.com>
2025-04-01 21:52:45 +0300

    comlited task03

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "89.169.173.119"]
 
 
 # Application definition
